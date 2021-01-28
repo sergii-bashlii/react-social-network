566f2cd54c2b955e805063ee4963dea25d9218e9
Serhii Bashlii <sergey.b@splynx.com>
2021-01-28 13:59:49 +0200

    lesson-14-css-modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://i.pinimg.com/564x/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg' alt='logo'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import s from './Profile.module.css';
+
+const Profile = () => {
+    return <div className={s.content}>
+        <div>
+            <img
+                src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
+        </div>
+        <div>
+            Avatar + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+        </div>
+        <div className={s.posts}>
+            <div  className={s.item}>
+                Post 1
+            </div>
+            <div className={s.item}>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
