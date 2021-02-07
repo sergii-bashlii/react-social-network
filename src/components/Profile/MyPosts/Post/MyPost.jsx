07582e584bf0226888a66eb7998dac6239881884
Serhii Bashlii <sergey.b@splynx.com>
2021-02-07 15:03:37 +0200

    lesson-20-NavLink-component

--- src/components/Profile/MyPosts/Post/MyPost.jsx
+++ src/components/Profile/MyPosts/Post/MyPost.jsx
@@ -4,8 +4,6 @@ import s from './Post.module.css';
 const Post = (props) => {
 
 
-    debugger;
-
     return(
         <div className={s.item}>
             <img src='https://i.pinimg.com/originals/c2/af/09/c2af0941a9eace5f0ba3dc63284d3860.jpg' />
